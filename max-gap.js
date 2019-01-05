1e32fb0165a4e75443f8f4ef56349f16961049b4
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-01-05 11:16:24 -0500

    Refactor with cbro

--- max-gap.js
+++ max-gap.js
@@ -5,14 +5,12 @@
 const maxGap = (arr) => {
     arr.sort((a, b) => a - b);
     let maxDiff = 0;
-    if (arr.length === 0) {
+    if (arr.length < 2) {
         return 0;
     }
-    let prev = arr[0];
     for (let i = 1; i < arr.length; i++) {
-        let diff = arr[i] - prev;
+        let diff = arr[i] - arr[i-1];
         maxDiff = Math.max(maxDiff, diff);
-        prev = arr[i];
     }
     return maxDiff;
 }
@@ -24,6 +22,12 @@ if (maxGap([-20, 1, 17, -3, 16, 2, 7]) !== 17) {
     throw new Error();
 }
 if (maxGap([20, 1.1, 17, 3.5, -16, 2, 7]) !== 17.1) {
+    throw new Error();
+}
+if (maxGap([20]) !== 0) {
+    throw new Error();
+}
+if (maxGap([]) !== 0) {
     throw new Error();
 }
 
