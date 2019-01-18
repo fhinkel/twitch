ea37197bc999351c6f1e7ccda0119dfc8699f606
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-01-18 12:22:24 -0800

    Initial import substring

---
+++ substring.js
@@ -0,0 +1,11 @@
+// Given a string s and an array of words, find that longest word 
+// that's a substring of s
+
+// Word w is a subsequence of s if some number of characters, possibly 
+// zero, can be deleted from s to form s, without 
+// reordering the remaining characters.
+
+// s = "abppplee" and 
+// words = {"able", "ale", "apple", "bale", "kangaroo"} 
+// the correct output would be "apple"
+
