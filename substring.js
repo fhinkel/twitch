bec313cd6160ce1c099f74651d189b4602870a41
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-01-19 11:24:56 -0500

    Add some tests

--- substring.js
+++ substring.js
@@ -1,11 +1,39 @@
 // Given a string s and an array of words, find that longest word 
-// that's a substring of s
+// that's a substring of s.
 
 // Word w is a subsequence of s if some number of characters, possibly 
-// zero, can be deleted from s to form s, without 
+// zero, can be deleted from s to form w, without 
 // reordering the remaining characters.
 
 // s = "abppplee" and 
 // words = {"able", "ale", "apple", "bale", "kangaroo"} 
 // the correct output would be "apple"
 
+// Assumption [a-z]
+
+const s = "abppplee";
+const words = ["able", "ale", "apple", "bale", "kangaroo"];
+
+const longestSubSequence = (s, words) => {
+    // words.sort((a, b)=> a-b)
+    words.sort((a, b) => (b.length - a.length));
+
+    
+
+    return '';
+}
+
+const test = (s, words, expected) => {
+    const res = longestSubSequence(s, words);
+    if(res !== expected) {
+        console.log(`${res} != ${expected}`);
+        throw new Error();
+    }
+}
+
+test(s, words, 'apple');
+test('asfkjwfpbhadslsegsfd', words, 'bale');
+test('', words, '');
+test(s, [], '');
+test(s, ['xxxx'], '');
+test(s, [s, ...words], s);
