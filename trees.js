010b99bfd6c4de2b49add26ad9b089688600658d
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-02-02 13:01:03 -0500

    Initial import of trees

---
+++ trees.js
@@ -0,0 +1,39 @@
+//        6
+//      /   \
+//     4      3
+//    /\       \  
+//   9  1       8
+
+// Inorder
+// Preorder
+// Postorder
+// Level Order Traversal
+
+
+// Given inorder and postorder traversal of a tree, construct the binary tree.
+// inorder = [9,3,15,20,7]
+// postorder = [9,15,7,20,3]
+
+
+// Given inorder and postorder traversal of a tree, construct the binary tree.
+// Note:
+// You may assume that duplicates do not exist in the tree.
+// inorder = [9,3,15,20,7]
+// postorder = [9,15,7,20,3]
+
+
+// Given a singly linked list where elements are 
+// sorted in ascending order, convert it to a height balanced BST.
+// For this problem, a height-balanced binary tree 
+// is defined as a binary tree in which the depth 
+// of the two subtrees of every node never differ by more than 1.
+// Given the sorted linked list: [-10,-3,0,5,9],
+
+
+// Given a binary tree in which each node 
+// element contains a number. Find the maximum 
+// possible sum from one leaf node to another.
+
+// Find the maximum distance
+
+
