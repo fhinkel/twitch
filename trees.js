4d6152d2ae23f6a1209c66242a6c53b1d0560e4d
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-02-02 13:29:11 -0500

    Add traversal methods

--- trees.js
+++ trees.js
@@ -1,14 +1,79 @@
 //        6
 //      /   \
 //     4      3
-//    /\       \  
-//   9  1       8
+//    /\      /\  
+//   9  1  null 8
+
+// Traversing the tree
+// Iterative, breadth-first search, level order traversal [6, 4, 3, 9, 1, null, 8]
+// Recursive, depth-first search
+// [6, 4, 9, 1, 3, null, 8] pre order
+// [9, 4, 1 , 6, null, 3, 8] in order
+// [9, 1, 4, null, 8, 3, 6] post order
+
+let node = {
+    val: 6,
+    left: null,
+    right: null
+}
+
+const root = { val: 6 };
+root.left = { val: 4 };
+root.right = { val: 3 };
+root.left.left = { val: 9 };
+root.left.right = { val: 1 };
+root.right = { val: 3 };
+root.right.right = { val: 8 };
 
 // Inorder
-// Preorder
-// Postorder
-// Level Order Traversal
+const inorder = (node) => {
+    if (node === undefined) {
+        return;
+    }
+    inorder(node.left);
+    console.log(node.val);
+    inorder(node.right);
+}
+inorder(root);
 
+// Preorder
+const preorder = (node) => {
+    if (node === undefined) {
+        return;
+    }
+    console.log(node.val);
+    preorder(node.left);
+    preorder(node.right);
+}
+preorder(root);
+
+// Postorder
+const postorder = (node) => {
+    if (node === undefined) {
+        return;
+    }
+    postorder(node.left);
+    postorder(node.right);
+    console.log(node.val);
+}
+postorder(root);
+
+// Level Order Traversal
+const levelOrder = (root) => {
+    const queue = [];
+
+    queue.push(root);
+    while (queue.length !== 0) {
+        const node = queue.shift();
+        if (node === undefined) {
+            continue;
+        }
+        console.log(node.val);
+        queue.push(node.left, node.right);
+    }
+}
+
+levelOrder(root);
 
 // Given inorder and postorder traversal of a tree, construct the binary tree.
 // inorder = [9,3,15,20,7]
