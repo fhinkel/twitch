58dfa1ba3c04f39b3ddd6a91dc76dfa89f5ced7f
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-02-09 12:29:55 -0500

    Linear solution for trapped water problem

--- waterTrapped.js
+++ waterTrapped.js
@@ -5,3 +5,24 @@
 // Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 // Output: 6
 
+const leftMax = (a) => {
+    const max = [a[0]];
+    for (let i = 1; i < a.length; i++) {
+        max[i] = Math.max(max[i - 1], a[i]);
+    }
+    return max;
+}
+
+// O(n)
+const water = (a) => {
+    let w = 0;
+    const lefts = leftMax(a);
+    const rights = leftMax(a.reverse()).reverse();
+    for (let i = 0; i < a.length; i++) {
+        const waterHeight = Math.min(lefts[i], rights[i]);
+        w += waterHeight - a[i];
+    }
+    return w;
+}
+
+console.log(water([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
